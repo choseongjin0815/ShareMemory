08d0d5ffddc4df7bb0c4019b1e4d38490034d849
choseongin0815 <chobocho0815@gmail.com>
2025-01-30 19:03:25 +0900

    first-commit

---
+++ ShareMemory-back-end/src/main/java/com/chobocho/ShareMemory_back_end/ShareMemoryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.chobocho.ShareMemory_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMemoryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMemoryBackEndApplication.class, args);
+	}
+
+}
---
+++ ShareMemory-back-end/src/test/java/com/chobocho/ShareMemory_back_end/ShareMemoryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chobocho.ShareMemory_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMemoryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
